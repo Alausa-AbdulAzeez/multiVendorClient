ae2c0e8cce5b51938942dcc2d522ad6bcc2043ee
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-20 21:23:19 +0100

    configured rtk and redux persist

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { persistStore } from "redux-persist";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+let persistor = persistStore(store);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
 
---
+++ src/redux/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  loginStart: false,
+  loginSuccess: false,
+  loginFailure: false,
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginStart: (state) => {
+      state.loginStart = true;
+    },
+    loginSuccess: (state, action) => {
+      (state.loginStart = false), (state.loginSuccess = true);
+      state.user = action.payload;
+    },
+    loginFailure: (state) => {
+      state.loginStart = false;
+      state.loginFailure = true;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { loginFailure, loginStart, loginSuccess } = userSlice.actions;
+
+export default counterSlice.reducer;

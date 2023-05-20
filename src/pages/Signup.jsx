b69136e11580c3f8ba91cb2ec1b55dd9f33d38ca
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-20 19:51:19 +0100

    handled form data change

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,36 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { BASE_URL } from "../functions/requestMethods";
 
 const Signup = () => {
+  // FORM DATA
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  // END OF FORM DATA
+
+  // FUNCTION THAT HANDLES THE CHANGE OF FORM DATA
+  const handleFormDataChange = (e, dataType) => {
+    if (dataType === "name") {
+      setName(e.target.value);
+    }
+    if (dataType === "email") {
+      setEmail(e.target.value);
+    }
+    if (dataType === "password") {
+      setPassword(e.target.value);
+    }
+    if (dataType === "confirmPassword") {
+      setConfirmPassword(e.target.value);
+    }
+  };
+
+  console.log(name, email, password);
+  // END OF FUNCTION THAT HANDLES THE CHANGE OF FORM DATA
+
   // FUNCTIONALITIES FOR PASSWORD TOGGLE
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
@@ -18,7 +46,34 @@ const Signup = () => {
   const handleToggleConfirmPassword = () => {
     setIsConfirmPasswordVisible((prev) => !prev);
   };
-  // END OF FUNCTIONALITIES FOR CONFIRM     PASSWORD TOGGLE
+  // END OF FUNCTIONALITIES FOR CONFIRM PASSWORD TOGGLE
+
+  // FUNCTION TO REGISTER USER
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    // header config
+    const configuration = {
+      headers: { "Content-Type": "multipart/form-data" },
+    };
+    // FORM DETAILS SETTING FUNCTIONALITIES
+
+    const formDetails = new FormData();
+
+    formDetails.append("name", name);
+    formDetails.append("email", email);
+    formDetails.append("password", password);
+
+    // END OF FORM DETAILS SETTING FUNCTIONALITIES
+    for (const value of formDetails.values()) {
+      console.log(value);
+    }
+    // try {
+    //   axios.post(`${BASE_URL}/user/register`, formDetails, configuration);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+  // END OF FUNCTION TO REGISTER USER
 
   return (
     <div className="flex flex-col items-center h-screen py-8 bg-gray-50 sm:px-6 lg:px-8">
@@ -33,34 +88,39 @@ const Signup = () => {
           </label>
           <input
             type="text"
+            id="fullName"
             placeholder="John Doe"
             className="border border-gray-200 p-2 mt-1 text-sm focus:outline-none focus:border-blue-300 rounded"
+            onChange={(e) => handleFormDataChange(e, "name")}
           />
         </div>
         <div className="flex flex-col ">
-          <label
-            htmlFor="fullName"
-            className="text-sm font-medium text-gray-700"
-          >
-            Full Name
+          <label htmlFor="email" className="text-sm font-medium text-gray-700">
+            Email address
           </label>
           <input
-            type="text"
-            placeholder="John Doe"
+            type="email"
+            name="email"
+            id="email"
+            required
+            placeholder="example@gmail.com"
             className="border border-gray-200 p-2 mt-1 text-sm focus:outline-none focus:border-blue-300 rounded"
+            onChange={(e) => handleFormDataChange(e, "email")}
           />
         </div>
         <div className="flex flex-col relative">
           <label
-            htmlFor="fullName"
+            htmlFor="password"
             className="text-sm font-medium text-gray-700"
           >
             Password
           </label>
           <input
+            id="password"
             type={isPasswordVisible ? "text" : "password"}
             placeholder="Password"
             className="border border-gray-200 p-2 mt-1 text-sm focus:outline-none focus:border-blue-300 rounded"
+            onChange={(e) => handleFormDataChange(e, "password")}
           />
           {isPasswordVisible ? (
             <AiOutlineEye
@@ -76,15 +136,17 @@ const Signup = () => {
         </div>
         <div className="flex flex-col relative">
           <label
-            htmlFor="fullName"
+            htmlFor="confirmPassword"
             className="text-sm font-medium text-gray-700"
           >
             Confirm Password
           </label>
           <input
+            id="confirmPassword"
             type={isConfirmPasswordVisible ? "text" : "password"}
             placeholder="Password"
             className="border border-gray-200 p-2 mt-1 text-sm focus:outline-none focus:border-blue-300 rounded"
+            onChange={(e) => handleFormDataChange(e, "confirmPassword")}
           />
           {isConfirmPasswordVisible ? (
             <AiOutlineEye
@@ -98,7 +160,10 @@ const Signup = () => {
             />
           )}
         </div>
-        <button className="bg-blue-500 w-full rounded py-2 font-semibold text-sm text-white hover:bg-blue-600">
+        <button
+          className="bg-blue-500 w-full rounded py-2 font-semibold text-sm text-white hover:bg-blue-600"
+          onClick={handleSignUp}
+        >
           Submit
         </button>
         <div className="text-sm">

5af5f0e6e65b8c0300eb03130d85affc3b557173
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-28 09:02:49 +0100

    handled errors

--- src/pages/UserActivationPage.jsx
+++ src/pages/UserActivationPage.jsx
@@ -1,11 +1,14 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { BASE_URL, defaultConfiguration } from "../functions/requestMethods";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../functions/requestMethods";
 import { useParams } from "react-router-dom";
 
 const UserActivationPage = () => {
   // URL PARAMS
   let { activation_token } = useParams();
+
+  // RESPONSE VARIABLE
+  const [response, setResponse] = useState(null);
 
   // FUNCTION TO CALL THE ACTIVATE USER ENDPOINT
   const activateUser = async () => {
@@ -25,17 +28,34 @@ const UserActivationPage = () => {
             },
           }
         )
-        .then((res) => console.log(res));
+        .then((res) => {
+          console.log(res);
+          setResponse(res);
+        });
     } catch (error) {
+      setResponse(error);
       console.log(error);
     }
   };
 
+  // USEEFFECT TO CALL THE ACTIVATE USER FUNCTION
   useEffect(() => {
     activateUser();
+    // eslint-disable-next-line
   }, []);
+
+  // USEEFFECT TO UPDATE RESPONSE STATE
+  // useEffect(() => {
+  //   console.log(response);
+  // }, [response]);
   return (
-    <div className="flex items-center h-screen bg-red-200 justify-center"></div>
+    <div className="flex items-center h-screen bg-blue-100 justify-center">
+      {response?.response?.status === 409 || response?.response?.status === 200
+        ? "Account has been seccessfylly activated"
+        : response?.response?.data?.message ||
+          (response?.data?.message === "jwt expired" &&
+            "The token you provided has expired, please try again with a fresh token")}
+    </div>
   );
 };
 
